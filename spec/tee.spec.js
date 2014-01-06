b53ef75d7e25b8690108c52173aae1b62c724080
Alan Plum <me@pluma.io>
2014-01-06 17:13:28 +0100

    Added testcase for qtils.tee failing.

--- spec/tee.spec.js
+++ spec/tee.spec.js
@@ -1,5 +1,6 @@
 /*global describe, it */
 var expect = require('expect.js'),
+  Q = require('q'),
   tee = require('../').tee;
 
 describe('qtils.tee(fn)', function() {
@@ -7,15 +8,25 @@ describe('qtils.tee(fn)', function() {
     expect(tee()).to.be.a('function');
   });
   it('returns a promise resolving to its input', function(done) {
-    tee(function() {})(5)
+    Q(5)
+    .then(tee(function() {}))
     .done(function(result) {
       expect(result).to.equal(5);
       done();
     });
   });
+  it('returns a promise that is rejected when the tee fails', function(done) {
+    Q(5)
+    .then(tee(function() {throw new Error('Fail');}))
+    .done(null, function(reason) {
+      expect(reason).to.be.an(Error);
+      done();
+    });
+  });
   it('calls the passed function', function(done) {
     var called = false;
-    tee(function() {called = true;})()
+    Q()
+    .then(tee(function() {called = true;}))
     .done(function() {
       expect(called).to.equal(true);
       done();
